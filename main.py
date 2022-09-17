355bb937acd527a72d12266c75429ebb162996ac
Recepce <k.klempir@gmail.com>
2022-09-17 17:56:20 +0200

    bug spottong 2

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ car_manager = CarManager
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
-
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
@@ -23,3 +22,8 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
